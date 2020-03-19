aaa36acc144170a93c6f9f29732f74d129714f08
kriviears <kriviears@gmail.com>
2020-03-19 17:34:22 -0400

    Landing page fixed

--- frame-data-client/src/LandingPage/LandingPage.js
+++ frame-data-client/src/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./LandingPage.css";
 
 export default class LandingPage extends Component {
@@ -6,9 +7,16 @@ export default class LandingPage extends Component {
     return (
       <div>
         <h1>Welcome to the Frame data app for Samurai Shodown.</h1>
-        <h2>Click the button to select a character</h2>
+        <p>
+          'Frame data' explains the timing of when characters in a fighting game
+          are allowed to act again after an interaction
+        </p>
+        <p>
+          This app charts out the different aspects of each move of the
+          characters in Samurai Shodown
+        </p>
         <Link className="Start" to={"/select"}>
-          <h1>Click here to select a character</h1>
+          <h2>Click here to begin and select a character</h2>
         </Link>
       </div>
     );
